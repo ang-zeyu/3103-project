089e04d6e0c0e3bbf7dd8c2de91d72c69788f16f
choo-xing-yu <chooxingyu@hotmail.com>
2021-11-05 18:56:28 +0800

    use namespace std

--- assignment2/jobScheduler.cpp
+++ assignment2/jobScheduler.cpp
@@ -1,3 +1,5 @@
+using namespace std;
+
 #include <arpa/inet.h>
 #include <stdio.h>
 #include <string.h>
@@ -16,23 +18,23 @@
 
 // KeyboardInterrupt handler
 void signalHandler(int signum) {
-    std::cout << "Interrupt signal (" << signum << ") received.\n";
-    std::exit(signum);
+    cout << "Interrupt signal (" << signum << ") received.\n";
+    exit(signum);
 }
 
 // send trigger to printAll at servers
 void sendPrintAll(const int& serverSocket) {
-    std::string message = "printAll\n";
+    string message = "printAll\n";
     send(serverSocket, message.c_str(), strlen(message.c_str()), 0);
 }
 
 // For example, "1\n2\n3\n4\n5\n" -> "1","2","3","4","5"
 // Be careful that "1\n2\n3" -> "1,2" without 3.
-std::vector<std::string> parseWithDelimiter(std::string target, std::string delimiter) {
-    std::string element;
-    std::vector<std::string> ret;
+vector<string> parseWithDelimiter(string target, string delimiter) {
+    string element;
+    vector<string> ret;
     size_t pos = 0;
-    while ((pos = target.find(delimiter)) != std::string::npos) {
+    while ((pos = target.find(delimiter)) != string::npos) {
         element = target.substr(0, pos);
         ret.push_back(element);
         target.erase(0, pos + delimiter.length());
@@ -41,17 +43,17 @@ std::vector<std::string> parseWithDelimiter(std::string target, std::string deli
 }
 
 // Parse available severnames
-std::vector<std::string> parseServernames(char* buffer, int len) {
+vector<string> parseServernames(char* buffer, int len) {
     printf("Servernames: %s\n", buffer);
-    std::string servernames(buffer);
+    string servernames(buffer);
 
     // parse with delimiter ","
-    std::vector<std::string> ret = parseWithDelimiter(servernames, ",");
+    vector<string> ret = parseWithDelimiter(servernames, ",");
     return ret;
 }
 
 // get the completed file's name, what you want to do?
-void getCompletedFilename(std::string filename) {
+void getCompletedFilename(string filename) {
     /****************************************************
      *                       TODO                       *
      * You should use the information on the completed  *
@@ -69,26 +71,26 @@ void getCompletedFilename(std::string filename) {
     /**************************************************/
 }
 
-std::string parser_filename(std::string request) {
-    std::vector<std::string> parsed = parseWithDelimiter(request + ",", ",");
-    std::string filename = parsed[0];
+string parser_filename(string request) {
+    vector<string> parsed = parseWithDelimiter(request + ",", ",");
+    string filename = parsed[0];
     return filename;
 }
 
 // parser of request to 2-tuple
-int parser_jobsize(std::string request) {
-    std::vector<std::string> parsed = parseWithDelimiter(request + ",", ",");
-    int jobsize = std::stoi(parsed[1]);
+int parser_jobsize(string request) {
+    vector<string> parsed = parseWithDelimiter(request + ",", ",");
+    int jobsize = stoi(parsed[1]);
     return jobsize; // it can be -1 (i.e., unknown)
 }
 
 // formatting: to assign server to the request
-std::string scheduleJobToServer(std::string servername, std::string request) {
-    return servername + std::string(",") + request + std::string("\n");
+string scheduleJobToServer(string servername, string request) {
+    return servername + string(",") + request + string("\n");
 }
 
 // main part you need to do
-std::string assignServerToRequest(std::vector<std::string> servernames, std::string request) {
+string assignServerToRequest(vector<string> servernames, string request) {
     /****************************************************
      *                       TODO                       *
      * Given the list of servers, which server you want *
@@ -96,7 +98,7 @@ std::string assignServerToRequest(std::vector<std::string> servernames, std::str
      * You can use a global variables or add more       *
      * arguments.                                       */
 
-    std::string request_name = parser_filename(request);
+    string request_name = parser_filename(request);
     int request_size = parser_jobsize(request);
 
     /** 
@@ -105,27 +107,27 @@ std::string assignServerToRequest(std::vector<std::string> servernames, std::str
      */
 
     /* Example. always assign to the first server */
-    std::string server_to_send = servernames[0];
+    string server_to_send = servernames[0];
 
     /**************************************************/
 
     /* Schedule the job */
-    std::string scheduled_request = scheduleJobToServer(server_to_send, request);
+    string scheduled_request = scheduleJobToServer(server_to_send, request);
     return scheduled_request;
 }
 
-void parseThenSendRequest(char* buffer, int len, const int& serverSocket, std::vector<std::string> servernames) {
+void parseThenSendRequest(char* buffer, int len, const int& serverSocket, vector<string> servernames) {
     // print received requests
     printf("[JobScheduler] Received string messages:\n%s\n", buffer);
     printf("[JobScheduler] --------------------\n");
-    std::string sendToServers;
+    string sendToServers;
 
     // parsing to "filename, jobsize" pairs
-    std::vector<std::string> request_pairs = parseWithDelimiter(std::string(buffer), "\n");
+    vector<string> request_pairs = parseWithDelimiter(string(buffer), "\n");
     for (const auto& request : request_pairs) {
-        if (request.find("F") != std::string::npos) {
+        if (request.find("F") != string::npos) {
             // if completed filenames, print them
-            std::string completed_filename = std::regex_replace(request, std::regex("F"), "");
+            string completed_filename = regex_replace(request, regex("F"), "");
             getCompletedFilename(completed_filename);
         } else {
             // if requests, add "servername" front of the request pair
@@ -141,10 +143,10 @@ int main(int argc, char const* argv[]) {
     signal(SIGINT, signalHandler);
 
     if (argc != 2) {
-        throw std::invalid_argument("must type port number");
+        throw invalid_argument("must type port number");
         return -1;
     }
-    uint32_t portNumber = std::stoi(std::string(argv[1]));
+    uint32_t portNumber = stoi(string(argv[1]));
 
     int serverSocket = 0;
     struct sockaddr_in serv_addr;
@@ -182,10 +184,10 @@ int main(int argc, char const* argv[]) {
     int len;
 
     len = read(serverSocket, buffer, 4096);
-    std::vector<std::string> servernames = parseServernames(buffer, len);
+    vector<string> servernames = parseServernames(buffer, len);
 
     int currSeconds = -1;
-    auto now = std::chrono::system_clock::now();
+    auto now = chrono::system_clock::now();
     while (true) {
         try {
             len = read(serverSocket, buffer, 4096);
@@ -194,12 +196,12 @@ int main(int argc, char const* argv[]) {
                 memset(buffer, 0, 4096);
             }
             sleep(0.00001);  // sufficient for 50ms granualrity
-        } catch (const std::exception& e) {
-            std::cerr << e.what() << '\n';
+        } catch (const exception& e) {
+            cerr << e.what() << '\n';
         }
 
         // // Example printAll API : let servers print status in every seconds
-        // if (std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now() - now).count() > currSeconds) {
+        // if (chrono::duration_cast<chrono::seconds>(chrono::system_clock::now() - now).count() > currSeconds) {
         //     currSeconds = currSeconds + 1;
         //     sendPrintAll(serverSocket);
         // }
